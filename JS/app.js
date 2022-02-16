d5cdd39792ca834f2648c8a13f69bec97e22e800
Md Arfan Khan <khan188993@gmail.com>
2022-02-17 00:12:39 +0600

    js work starts

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+//Input Selector
+const totalIncome = document.getElementById('total-income');
+const totalIncome = document.getElementById('food-expanse');
+const totalIncome = document.getElementById('rent-expanse');
+const totalIncome = document.getElementById('clothes-income');
+const totalExpanse = document.getElementById('total-expanse');
+const savingPercentage = document.getElementById('saving-percentage');
+
+//Money Selector 
+const netBalance = document.getElementById('net-balance');
+const totalIncome = document.getElementById('total-income');
+const savingMoney = document.getElementById('saving-money');
+const remainingBalance = document.getElementById('remaining-balance');
+
+// button Selector
+const expanseCalculateButton = document.getElementById('expanse-calculate')
+const savingCalculateButton = document.getElementById('savings')
+

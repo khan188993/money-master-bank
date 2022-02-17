09b4acedeef07b1f1eda4e28b4c2e508bc6d415e
Md Arfan Khan <khan188993@gmail.com>
2022-02-17 14:27:09 +0600

    fixed text spelling

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
 //Input Selector
 const totalIncome = document.getElementById("total-income");
-const foodExpanse = document.getElementById("food-expanse");
-const rentExpanse = document.getElementById("rent-expanse");
-const clothesExpanse = document.getElementById("clothes-expanse");
+const foodExpense = document.getElementById("food-expense");
+const rentExpense = document.getElementById("rent-expense");
+const clothesExpense = document.getElementById("clothes-expense");
 const savingPercentage = document.getElementById("saving-percentage");
 
 //Money Showing Selector
 const netBalance = document.getElementById("net-balance");
-const totalExpanse = document.getElementById("total-expanse");
+const totalExpense = document.getElementById("total-expense");
 const savingMoney = document.getElementById("saving-money");
 const remainingBalance = document.getElementById("remaining-balance");
 
@@ -18,8 +18,8 @@ const savingCalculateButton = document.getElementById("savings");
 // Initial Total Balance
 let TotalBalance = 0;
 
-//Total Expanses Calculating
-function totalExpanseCalculate(food, rent, clothes) {
+//Total Expenses Calculating
+function totalExpenseCalculate(food, rent, clothes) {
     return Number(food) + Number(rent) + Number(clothes);
 }
 
@@ -33,25 +33,25 @@ function warningMessage(text, parent) {
 
     //If got any errors make all balance into 0;
     netBalance.innerText = 0;
-    totalExpanse.innerText = 0;
+    totalExpense.innerText = 0;
     savingMoney.innerText = 0;
     remainingBalance.innerText = 0;
 
 }
 
 //Input Number Type Checking
-function inputNumberTypeCheck(totalIncome, foodExpanse, rentExpanse, clothesExpanse) {
-    if (totalIncome.value === "" || foodExpanse.value === "" || rentExpanse.value === "" || clothesExpanse.value === "") {
+function inputNumberTypeCheck(totalIncome, foodExpense, rentExpense, clothesExpense) {
+    if (totalIncome.value === "" || foodExpense.value === "" || rentExpense.value === "" || clothesExpense.value === "") {
         warningMessage("please fill all the input fields properly", document.querySelector(".clothes"));
     } else if (isNaN(totalIncome.value)) {
         warningMessage("total income input must be a number", document.querySelector(".total-income"));
-    } else if (isNaN(foodExpanse.value)) {
-        warningMessage("food expanse input must be a number", document.querySelector(".food"));
-    } else if (isNaN(rentExpanse.value)) {
-        warningMessage("rent expanse input must be a number", document.querySelector(".rent"));
-    } else if (isNaN(clothesExpanse.value)) {
-        warningMessage("clothes expanse input must be a number", document.querySelector(".clothes"));
-    } else if (totalIncome.value < 0 || foodExpanse.value < 0 || rentExpanse.value < 0 || clothesExpanse.value < 0) {
+    } else if (isNaN(foodExpense.value)) {
+        warningMessage("food expense input must be a number", document.querySelector(".food"));
+    } else if (isNaN(rentExpense.value)) {
+        warningMessage("rent expense input must be a number", document.querySelector(".rent"));
+    } else if (isNaN(clothesExpense.value)) {
+        warningMessage("clothes expense input must be a number", document.querySelector(".clothes"));
+    } else if (totalIncome.value < 0 || foodExpense.value < 0 || rentExpense.value < 0 || clothesExpense.value < 0) {
         warningMessage("all input value must be greater than 0 ", document.querySelector(".clothes"));
     } else {
         return true;
@@ -62,22 +62,22 @@ function inputNumberTypeCheck(totalIncome, foodExpanse, rentExpanse, clothesExpa
 
 
 
-//Calculating Expanse and net Balance
+//Calculating Expense and net Balance
 
 CalculateButton.addEventListener("click", function() {
-    let inputNumberTypeResult = inputNumberTypeCheck(totalIncome, foodExpanse, rentExpanse, clothesExpanse);
+    let inputNumberTypeResult = inputNumberTypeCheck(totalIncome, foodExpense, rentExpense, clothesExpense);
     TotalBalance = Number(totalIncome.value);
 
     //if inputNumberTypeResult pass then Execute
     if (inputNumberTypeResult) {
-        let totalExpanseCalc = totalExpanseCalculate(foodExpanse.value, rentExpanse.value, clothesExpanse.value);
-        if (totalExpanseCalc > TotalBalance) {
-            warningMessage("your expanses can't be greater than your total income", document.querySelector(".clothes"));
+        let totalExpenseCalc = totalExpenseCalculate(foodExpense.value, rentExpense.value, clothesExpense.value);
+        if (totalExpenseCalc > TotalBalance) {
+            warningMessage("your expenses can't be greater than your total income", document.querySelector(".clothes"));
 
         } else {
             document.getElementById("warning") && document.getElementById("warning").remove();
-            netBalance.innerText = TotalBalance - totalExpanseCalc;
-            totalExpanse.innerText = totalExpanseCalc;
+            netBalance.innerText = TotalBalance - totalExpenseCalc;
+            totalExpense.innerText = totalExpenseCalc;
         }
     }
 
@@ -95,7 +95,7 @@ savingCalculateButton.addEventListener("click", function() {
             savingMoney.innerText = Math.round(savingAmount);
             remainingBalance.innerText = Math.round(remainingBalanceAmount);
         } else {
-            warningMessage("savings can't be greater than your net income", document.querySelector(".saving-calculate"));
+            warningMessage("savings can't be greater than your net balance", document.querySelector(".saving-calculate"));
         }
     } else {
         warningMessage("savings percentage can't be less than 0 and greater than 100", document.querySelector(".saving-calculate"));
